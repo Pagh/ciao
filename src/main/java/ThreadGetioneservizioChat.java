afaaf6698cda61d5fd35a21845a79726431befc0
lai2 <informatica@lai2>
2020-10-20 08:59:10 +0200

    first commit

---
+++ src/main/java/ThreadGetioneservizioChat.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+
+public class ThreadGetioneservizioChat {
+    private int nrMaxConnessioni;
+    private List lista;
+    private ThreadChatConnessione[] listaConnessioni;
+    
+    public ThreadGetioneservizioChat(int numeroMaxConnessioni, List lista){
+        this.nrMaxConnessioni = nrMaxConnessioni - 1;
+        this.lista = lista;
+        private ThreadChatConnessione[] listaConnessioni;
+        Thread me;
+        private ServerSoocket serverChat;
+        
+    }
+
+    public ThreadGestioneservizioChat(int numeroMaxConnessioni, List lista){
+
+    this.nrMaxConnessioni = nrMaxConnesioni - 1;
+    
+    
+}
+    
+}
