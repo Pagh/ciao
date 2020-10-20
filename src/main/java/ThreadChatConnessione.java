1f627df63682da651a75b2d8a6647158beda7263
lai2 <informatica@lai2>
2020-10-20 09:28:05 +0200

    Second commit

--- src/main/java/ThreadChatConnessione.java
+++ src/main/java/ThreadChatConnessione.java
@@ -1,3 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ThreadChatConnessione {
+    private ThreadGestioneservizioChat gestoreChat;
+    private Socket client = null;
+    private BufferedReader input = null;
+    private PrintWriter output = null;
+    Thread me;
+    
+    public ThreadChatConnessione(ThreadGestioneservizioChat gestoreChat, Socket client){
+        this.gestoreChat = gestoreChat;
+        this.client = client;
+        try{
+            this.input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            this.output = new PrintWriter(thi.client.getOutputStream(), true);
+        }catch (Exception e){
+            output.println("Errore nella lettura");
+        }
+        me = new Thread(this);
+        me.start();
+    }
     
 }
--- src/main/java/ThreadGetioneservizioChat.java
+++ src/main/java/ThreadGetioneservizioChat.java
@@ -1,25 +1,57 @@
 
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class ThreadGetioneservizioChat {
     private int nrMaxConnessioni;
     private List lista;
     private ThreadChatConnessione[] listaConnessioni;
+    Thread me;
+    private ServerSocket serverChat;
+    
+    public void run(){
+        boolean continua = true;
+        
+        try{
+            serverChat = new ServerSocket(6789);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "impossibile instanziare il server");
+            continua = false;
+        }
+        
+        if(continua){
+            try{
+                for(int xx = 0; xx < nrMaxConnessioni; xx++){
+                    Socket tempo = null;
+                    tempo = serverChat.accept();
+                    listaConnessioni[xx] = new ThreadChatConnessione(this.tempo);
+                }
+                sererChat.close();
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Impossibile instaurare server chat");
+            }
+        }
+    }
+    public void pedisciMessaggio(strig mex){
+        lista.add(mex);
+        lista.select(lista.getItemCount() - 1);
+        for(int xx = 0; xx < rhis.nrMaxConnessioni; xx++){
+            if(listaConnessioni[xx] != null){
+                listaConnessioni[xx].spedisciMessaggioChat(mex);
+            }
+        }
+    }
     
     public ThreadGetioneservizioChat(int numeroMaxConnessioni, List lista){
         this.nrMaxConnessioni = nrMaxConnessioni - 1;
         this.lista = lista;
-        private ThreadChatConnessione[] listaConnessioni;
-        Thread me;
+        this.listaConnessioni = new ThreadChatConnessione[this.nrMaxConnessioni];
+        me = new Thread(this);
+        me.start();
         private ServerSoocket serverChat;
-        
     }
-
-    public ThreadGestioneservizioChat(int numeroMaxConnessioni, List lista){
-
-    this.nrMaxConnessioni = nrMaxConnesioni - 1;
     
+
     
-}
     
 }
